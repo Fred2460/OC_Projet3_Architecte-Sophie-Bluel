de53645ffd402d6193eba7ea4e03b81a7165a946
Frederic FLIPO <fred.web24@gmail.com>
2024-07-23 11:31:51 +0200

    etape 1.1 Recupération des travaux depuis le back-end

---
+++ FrontEnd/projets.js
@@ -0,0 +1,48 @@
+//Récupération des projets eventuellement stockés dans le localStorage
+let projets = window.localStorage.getItem('projets');
+
+if (projets === null) {
+    // Récupération des projets depuis l'API
+    const reponse = await fetch('http://localhost:5678/api/works/');
+    projets = await reponse.json();
+    // Transformation des projets en JSON
+    const valeurProjets = JSON.stringify(projets);
+    // Stockage des informations dans le localStorage
+    window.localStorage.setItem("projets", valeurProjets);
+} else {
+    projets = JSON.parse(projets);
+}
+
+function genererProjets(projets) {
+    for (let i = 0; i <= projets.length; i++) {
+
+        const projet = projets[i];
+        // Récupération de l'élément du DOM qui accueillera la gallerie
+        const divGallery = document.querySelector(".gallery");
+        // Création d’une balise dédiée à un projet
+        const idProjet = document.createElement("figure");
+        idProjet.dataset.id = projets[i].id;
+        // Création des balises 
+        const imageProjet = document.createElement("img");
+        imageProjet.src = projet.imageUrl;
+        const titreProjet = document.createElement("figcaption");
+        titreProjet.innerText = projet.title;
+        const useridProjet = document.createElement("p");
+        useridProjet.innerText = projet.userId;
+        const categoryidProjet = document.createElement("p");
+        categoryidProjet.innerText = projet.categoryId;
+
+        // Rattachement de la balise projet a la division Gallery
+        divGallery.appendChild(idProjet);
+        idProjet.appendChild(imageProjet);
+        idProjet.appendChild(titreProjet);
+        idProjet.appendChild(useridProjet);
+        idProjet.appendChild(categoryidProjet);
+
+    }
+}
+
+//genererProjets(projetElement);
+genererProjets(projets);
+
+console.log(idProjet);
