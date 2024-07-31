3cac3cabea6cc8747301ffe42ccc6240f5d311ca
Frederic FLIPO <fred.web24@gmail.com>
2024-07-31 12:22:06 +0200

    etape 1.2 filtre par categorie (partiel) et affichage gallerie

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,3 +1,4 @@
+// ************** PROJETS *****************
 //Récupération des projets eventuellement stockés dans le localStorage
 let projets = window.localStorage.getItem('projets');
 
@@ -39,9 +40,8 @@ function genererProjets(projets) {
         idProjet.appendChild(titreProjet);
     }
 }
-document.querySelector(".gallery").innerHTML = "";
-genererProjets(projets);
 
+// ************** CATEGORIES *****************
 //Récupération des catégories éventuellement stockées dans le localStorage
 let categories = window.localStorage.getItem('categories');
 
@@ -57,21 +57,22 @@ if (categories === null) {
     categories = JSON.parse(categories);
 }
 
-function genererFiltre(categories) {
-    
-    // Ajout du bouton "Tous" dans tableau categories si inexistant
-    if (categories.includes("Tous") == false) {
-        const boutonTous = {
-            id: 0,
-            name: "Tous"
-        }
-        categories.push(boutonTous);
+// ************** FILTRE *****************
+// Ajout du bouton "Tous" dans tableau categories si inexistant
+console.log("categories initiales avant =", categories); // Vérif
+if (categories.includes("Tous") == false) {
+    const boutonTous = {
+        id: 0,
+        name: "Tous"
     }
+    categories.push(boutonTous);
+}
+console.log("categories initiales après =", categories); // Vérif
+
+function genererFiltre(categories, boutonOn) {
 
     // Tri des boutons Filtres selon l'id de la catégorie
     categories.sort((a, b) => a.id - b.id);
-
-//    console.log("catégories après test Tous après tri =", categories); // vérif
 
     for (let i = 0; i < categories.length; i++) {
         const categorie = categories[i];
@@ -83,145 +84,63 @@ function genererFiltre(categories) {
         nomCategorie.innerText = categorie.name;
 
         // Rattachement de la balise projet à la division Filtre
-        divFiltre.appendChild(nomCategorie);
-        nomCategorie.classList.add("btn-filtre");
-        
-        // Activation du bouton "Tous" par défaut
-        //if (categorie.name == "Tous") {
-        //    nomCategorie.classList.add("btn-on")
-        //}
+        if (nomCategorie == boutonOn) {
+            divFiltre.appendChild(nomCategorie);
+            nomCategorie.classList.add("btn-filtre btn-on");
+        } else {
+            divFiltre.appendChild(nomCategorie);
+            nomCategorie.classList.add("btn-filtre");
+        }
     }
 }
 
 console.log("catégories avant affichage =", categories); // vérif
-
+const boutonOn = "Tous";
 document.querySelector(".filtre").innerHTML = "";
-genererFiltre(categories);
+genererFiltre(categories, boutonOn);
+console.log("catégories après affichage =", categories); // vérif
 
-//gestion des boutons Filtre
-//const idBouton = "Tous";
+// ************** GESTION DES APPUIS BOUTONS FILTRE *****************
 
-
-
-
-
-
-
-
-//const boutonFiltrer = document.querySelector(".btn-filtre");
 const boutonFiltrer = document.querySelectorAll(".btn-filtre");
+
 const Bouton = "";
 for (let Bouton of boutonFiltrer) {
     Bouton.addEventListener("click", function () {
-        //console.log("boutonFiltrer =", boutonFiltrer);
-        //console.log("Bouton =", Bouton);
-        
-        const baliseBoutonTous = document.getElementById("Tous");
-        const baliseBoutonObjets = document.getElementById("Objets");
-        const baliseBoutonAppartements = document.getElementById("Appartements");
-        const baliseBoutonHotels = document.getElementById("Hotels & restaurants");
-        console.log("baliseBoutonTous avant boucle =", baliseBoutonTous) // Vérif
-        console.log("baliseBoutonObjets avant boucle =", baliseBoutonObjets) // Vérif
-
-        const baliseBoutonOn = document.getElementsByClassName(".btn-filtre btn-on");
-        console.log("Bouton on =", baliseBoutonOn);
         const nomBouton = Bouton.firstChild.nodeValue;
         console.log("Vérif nomBouton =", nomBouton);
 
         if (nomBouton != "Tous") {
-                const projetsFiltres = projets.filter(function (projet) {
-                    return projet.category.name == nomBouton;
-                });
-                console.log("Projets =", projets) // Vérif
-                console.log("Projets filtrés =", projetsFiltres) // Vérif
-                if (nomBouton == "Objets") {
-                    baliseBoutonTous.classList.remove("btn-on")
-                    baliseBoutonObjets.classList.add("btn-on")
-                    baliseBoutonAppartements.classList.remove("btn-on")
-                    baliseBoutonHotels.classList.remove("btn-on")
-                } else if (nomBouton == "Appartements") {
-                    baliseBoutonTous.classList.remove("btn-on")
-                    baliseBoutonObjets.classList.remove("btn-on")
-                    baliseBoutonAppartements.classList.add("btn-on")
-                    baliseBoutonHotels.classList.remove("btn-on")
-                } else if (nomBouton == "Hotels & restaurants") {
-                    baliseBoutonTous.classList.remove("btn-on")
-                    baliseBoutonObjets.classList.remove("btn-on")
-                    baliseBoutonAppartements.classList.remove("btn-on")
-                    baliseBoutonHotels.classList.add("btn-on")
-                }
-                document.querySelector(".gallery").innerHTML = "";
-                genererProjets(projetsFiltres);
-        } else {
-            baliseBoutonTous.classList.add("btn-on")
-            baliseBoutonObjets.classList.remove("btn-on")
-            baliseBoutonAppartements.classList.remove("btn-on")
-            baliseBoutonHotels.classList.remove("btn-on")
-            document.querySelector(".gallery").innerHTML = "";
-            genererProjets(projets);
-        }
-
-    });
-}    
-
-/*
-    const projetsFiltres = projets.filter(function (projet) {
-        return projet.category.name == idBouton;
-    });
-    document.querySelector(".gallery").innerHTML = "";
-    genererPieces(projetsFiltres);
-});
-*/
-
-
-/*
-for (let bouton of boutonFiltres) {
-    bouton.addEventListener("click", function () {
-        const idBouton = bouton.id;
-        console.log("Vérif dans boucle for, bouton.id :", bouton.id, " et idBouton :", idBouton);
-        //console.log("Vérif nomBouton", )
-        if ((idBouton != "Tous")) {
             const projetsFiltres = projets.filter(function (projet) {
-                return projet.category.name == idBouton;
+                return projet.category.name == nomBouton;
             });
-            if (idBouton == "Objets") {
-                baliseBoutonTous.classList.remove("btn-on")
-                baliseBoutonObjets.classList.add("btn-on")
-                baliseBoutonAppartements.classList.remove("btn-on")
-                baliseBoutonHotels.classList.remove("btn-on")
-            } else if (idBouton == "Appartements") {
-                baliseBoutonTous.classList.remove("btn-on")
-                baliseBoutonObjets.classList.remove("btn-on")
-                baliseBoutonAppartements.classList.add("btn-on")
-                baliseBoutonHotels.classList.remove("btn-on")
-            } else if (idBouton == "Hotels & restaurants") {
-                baliseBoutonTous.classList.remove("btn-on")
-                baliseBoutonObjets.classList.remove("btn-on")
-                baliseBoutonAppartements.classList.remove("btn-on")
-                baliseBoutonHotels.classList.add("btn-on")
-            }
+            console.log("projetsFiltres dans la boucle après tri =", projetsFiltres);
+            
+            // afficher la liste des catégories selon sélection filtre
+            document.querySelector(".filtre").innerHTML = "";
+            console.log("Génération filtre. categories = ", categories, "nomBouton", nomBouton); // Vérif
+            genererFiltre(categories, nomBouton);
+            
+            // afficher la liste des projets selon sélection filtre
+            document.querySelector(".gallery").innerHTML = "";
+            console.log("Génération gallery. projetsFiltres = ", projetsFiltres); // Vérif
+            genererProjets(projetsFiltres);
+            
+        } else {
+            const projetsFiltres = projets
+            console.log("projetsFiltres dans la boucle après tri =", projetsFiltres);
+
+            // afficher la liste des catégories avec Tous activé
+            document.querySelector(".filtre").innerHTML = "";
+            genererFiltre(categories, "Tous");
+
+            // afficher la liste de tous les projets
             document.querySelector(".gallery").innerHTML = "";
             genererProjets(projetsFiltres);
-        } else {
-        //    monChoix = 0;
-            baliseBoutonTous.classList.add("btn-on")
-            baliseBoutonObjets.classList.remove("btn-on")
-            baliseBoutonAppartements.classList.remove("btn-on")
-            baliseBoutonHotels.classList.remove("btn-on")
-            document.querySelector(".gallery").innerHTML = "";
-            genererProjets(projets);
         }
-        //activerBouton(monChoix)
-        console.log("idBouton après boucle =", idBouton)
-        console.log("boutonFiltres après boucle =", boutonFiltres)
-        console.log("baliseBoutonTous après boucle =", baliseBoutonTous)
-        console.log("baliseBoutonObjets après boucle =", baliseBoutonObjets)
     });
+}
 
-
-
-
-};*/
 
     //if Idbouton = 1 ("objet")...
     // voir avec filter peut-être, ajouter une classe pour modifer le display dans le css
