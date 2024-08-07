3f38de48b91b536b3c0e88bffd6bd91dbdaf9d43
Frederic FLIPO <fred.web24@gmail.com>
2024-08-07 18:03:17 +0200

    etape 2.2 authentification (partielle)

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,13 +1,32 @@
+// ************** RECUPERATION DES DONNEES DE CONNEXION **************
+const loginForm = document.getElementById("loginForm");
+const emailInput = document.getElementById("email");
+const mdpInput = document.getElementById("mdp");
+
+console.log("Elements du DOM sélectionnés (loginForm, email, mdp)=", loginForm, emailInput, mdpInput); // Vérif
+
 // ************** CONNEXION *****************
 
-//Récupération des projets eventuellement stockés dans le localStorage
-let users = window.localStorage.getItem("users");
+// Ecoute appuie bouton "Se connecter"
+loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault(); // empêche le comportement pas défaut
 
-if (users === null) {
+    // récupération des valeurs du formulaire
+    const email = emailInput.value;
+    const mdp = mdpInput.value;
+    console.log("valeurs récupérées (email, mdp)=", email, mdp); // Vérif
+
+
+//Récupération des projets eventuellement stockés dans le localStorage
+//let users = window.localStorage.getItem('users');
+
+//if (users === null) {
     // Connexion du user depuis l'API
     const reponse = await fetch('http://localhost:5678/api/users/login', {
         method: "POST",
         body: {
+        //    "email": email, // remplacé par valeur test en dur
+        //    "password": mdp // remplacé par valeur test en dur
             "email": "sophie.bluel@test.tld",
             "password": "S0phie"
         },
@@ -15,8 +34,14 @@ if (users === null) {
     users = await reponse.json();
     // Transformation des projets en JSON
     const valeurUsers = JSON.stringify(users);
+    console.log("users=", users); // Vérif
+    console.log("valeurUsers=", valeurUsers); // Vérif
+
     // Stockage des informations dans le localStorage
-    window.localStorage.setItem("users", valeurUsers);
-} else {
-    users = JSON.parse(users);
-};
+    //window.localStorage.setItem("users", valeurUsers);
+//} else {
+//    users = JSON.parse(users);
+//};
+});
+
+
