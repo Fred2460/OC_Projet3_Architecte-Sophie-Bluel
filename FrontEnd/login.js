13c51200bc4f6caa75fcc8574304f277f876a82b
Frederic FLIPO <fred.web24@gmail.com>
2024-08-08 18:49:49 +0200

    etape 2.2 authentification (partielle)

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,47 +1,91 @@
 // ************** RECUPERATION DES DONNEES DE CONNEXION **************
-const loginForm = document.getElementById("loginForm");
-const emailInput = document.getElementById("email");
-const mdpInput = document.getElementById("mdp");
+let loginForm = document.getElementById("loginForm");
+let emailInput = document.getElementById("email");
+let passwordInput = document.getElementById("password");
+let errorMessage = document.getElementById("errorMessage");
 
-console.log("Elements du DOM sélectionnés (loginForm, email, mdp)=", loginForm, emailInput, mdpInput); // Vérif
+console.log("Elements du DOM sélectionnés (loginForm, email, mdp)=", loginForm, emailInput, passwordInput); // Vérif
 
-// ************** CONNEXION *****************
+// ************** FONCTION CONNEXION *****************
+
+function genererLogin(email,password) {
+
+    fetch('http://localhost:5678/api/users/login', {
+        method: "POST",
+        headers: {
+            "contentyType": "application/json",
+            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcyMTcxMzQ3OSwiZXhwIjoxNzIxNzk5ODc5fQ.GZvbjtRRbRV_m-WyMDRApeHpt_elcWgOl85NJikmYDo"
+        },
+        body: {
+            "email": email,
+            "password": password
+            //    "email": "sophie.bluel@test.tld",
+            //    "password": "S0phie"
+        },
+        origin: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    })
+
+    .then (response => {
+        console.log("Réponse du serveur");
+        if (response.ok) {
+            return response.json()
+            .then (data => {
+                console.log("Succés :", data);
+                window.localStorage.setItem("usersId", data.userId); // stockage du userId récupéré
+                window.localStorage.setItem("token", data.token); // stockage du token récupéré
+                window.location.replace("index.html"); // redirection vers la page d'accueil
+            });
+        } else if (response.status === 401) {
+            console.log("Erreur 401: accès non autorisé");
+            errorMessage.textContent = "Erreur d'email ou de mot de passe";
+            // throw new Error("Réponse du réseau NOK");
+        } else if (response.status === 404) {
+            console.log("Erreur 404: utilisateur non trouvé");
+            errorMessage.textContent = "Erreur d'email ou de mot de passe";
+        }
+    })
+    
+    .catch(error => {
+        console.error("Il y a eu une erreur avec votre fetch :", error);
+        errorMessage.textContent = "Erreur d'accès au site, contactez votre administrateur";
+    });
+};
+
+// ************** LANCEMENT CONNEXION *****************
 
 // Ecoute appuie bouton "Se connecter"
 loginForm.addEventListener("submit", async (event) => {
     event.preventDefault(); // empêche le comportement pas défaut
 
     // récupération des valeurs du formulaire
-    const email = emailInput.value;
-    const mdp = mdpInput.value;
-    console.log("valeurs récupérées (email, mdp)=", email, mdp); // Vérif
+    let email = emailInput.value;
+    let password = passwordInput.value;
+    console.log("valeurs envoyées (email, password)=", email, password); // Vérif
 
+    let response = genererLogin(email, password);
+    console.log("Réponse du serveur=", response); // Vérif
+});
 
-//Récupération des projets eventuellement stockés dans le localStorage
-//let users = window.localStorage.getItem('users');
+/*
+console.log("Réponse du serveur=", response); // Vérif
+const useridLogin = response.userId;
+const usertokenLogin = response.token;
+console.log("useridLogin=", useridLogin); // Vérif
+console.log("usertokenLogin=", usertokenLogin); // Vérif
+*/
 
-//if (users === null) {
-    // Connexion du user depuis l'API
-    const reponse = await fetch('http://localhost:5678/api/users/login', {
-        method: "POST",
-        body: {
-        //    "email": email, // remplacé par valeur test en dur
-        //    "password": mdp // remplacé par valeur test en dur
-            "email": "sophie.bluel@test.tld",
-            "password": "S0phie"
-        },
-    });
-    users = await reponse.json();
+    //users = await reponse.json();
     // Transformation des projets en JSON
-    const valeurUsers = JSON.stringify(users);
-    console.log("users=", users); // Vérif
-    console.log("valeurUsers=", valeurUsers); // Vérif
-
+    //const valeurUsers = JSON.stringify(users);
+    //console.log("users=", users); // Vérif
+    //console.log("valeurUsers=", valeurUsers); // Vérif
+    //const useridLogin = valeurUsers.userId;
+    //const usertokenLogin = valeurUsers.token;
+    
     // Stockage des informations dans le localStorage
     //window.localStorage.setItem("users", valeurUsers);
 //} else {
 //    users = JSON.parse(users);
 //};
-});
-
-
